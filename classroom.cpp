fc5d527e09bb217d468788c0c46fedbfbfc29f5d
kok-sadra <sadrakasaeii@gmail.com>
2023-04-29 12:31:17 +0330

    start git from my side

---
+++ classroom.cpp
@@ -0,0 +1,6 @@
+//
+// Created by User on 28/04/2023.
+//
+
+#include "classroom.h"
+#include "numberclass.h"
---
+++ classroom.h
@@ -0,0 +1,15 @@
+//
+// Created by User on 28/04/2023.
+//
+
+#ifndef LOTFI_PROJECT_CLASSROOM_H
+#define LOTFI_PROJECT_CLASSROOM_H
+
+
+class classroom {
+
+
+};
+
+
+#endif //LOTFI_PROJECT_CLASSROOM_H
