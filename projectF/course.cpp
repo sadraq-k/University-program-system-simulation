a7b7642d3ba08a5d55f2c8998a1a8970f035038e
Foad <Foadgolabi@gmail.com>
2023-05-11 15:31:58 +0430

    some chanfe (commenting)

--- projectF/course.cpp
+++ projectF/course.cpp
@@ -116,12 +116,83 @@ public:
     coursetemp.close();
 }
 
+};
 
 
+
+//class classroom : etalaat kelas
+class classroom{ 
+   // friend void check();
+   // friend void inputClassroom(classroom& temp ,string help);
+protected:
+    string idClass;
+    int capacityClass;
+    bool videoprojector;
+    date dateClass;
+    string idCourse;
+
+
+public:
+
+    void show()
+    {
+        cout<<boolalpha;
+        cout<<idClass<<endl<<capacityClass<<endl<<videoprojector<<endl;
+    }
+
+    void inputClassroom(string help)
+{
+    char auxiliary[80];
+    char assist[80];
+
+    fstream classtemp;
+    classtemp.open("proj.txt" , ios::in);
+    if(classtemp.fail()){
+        cout<<"fail\tthe file has a problem"<<endl;
+    }
+    int j;
+    for(j = 1; !classtemp.eof(); j++ )
+    {
+        classtemp.getline(assist , 80);
+        if(assist == help)
+            break;
+    }
+    classtemp.close();
+    classtemp.open("proj.txt" , ios::in);
+
+    for(int i{1}; !classtemp.eof(); i++)
+    {
+        classtemp.getline(auxiliary , 80);
+        if(i>=j-1 && i<=j+6)
+        {
+
+            if(i == j)
+                idClass = auxiliary;
+            if(i == j+1)
+                capacityClass = atoi(auxiliary);
+            if(i == j+2)
+                videoprojector = stringToBool(auxiliary);
+
+
+            /* if(i == j+5)
+             {
+                 const char* x = auxiliary;
+                 temp.capacityCourse = atoi(x);
+             }*/
+
+
+        }
+
+    }
+    classtemp.close();
+}
 
 };
 
-// baraye mohasebe saat payan dars va kelas 
+
+
+
+// baraye mohasebe saat payan dars kelas
 auto calcuteTime(date object){
 
     object.minEnd = object.minStart + object.durationMin;
