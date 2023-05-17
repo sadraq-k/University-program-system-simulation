721c7ba4ec7d2e41ddede4facd1c2509093c43aa
kok-sadra <119508076+kok-sadra@users.noreply.github.com>
2023-05-17 13:00:02 +0330

    git from clion

---
+++ test1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by User on 5/15/2023.
+//
+
+#include "test1.h"
+
+void printSomething(int a){
+    std::cout<<a+1;
+}
+
+class a{
+
+};
---
+++ test1.h
@@ -0,0 +1,9 @@
+//
+// Created by User on 5/15/2023.
+//
+
+#ifndef UNIVERSITY_PROGRAM_SYSTEM_SIMULATION_TEST1_H
+#define UNIVERSITY_PROGRAM_SYSTEM_SIMULATION_TEST1_H
+#include <iostream>
+void printSomething(int);
+#endif //UNIVERSITY_PROGRAM_SYSTEM_SIMULATION_TEST1_H
